ff83fa4852cb92d63744b51f2a73934c7eb4a966
aymanhki <agamyahk@myumanitoba.ca>
2025-01-08 19:10:41 -0600

    Initial Commit

---
+++ FileFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FileFlow
+//
+//  Created by Ayman Agamy on 2025-01-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FileFlow/FileFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileFlowApp.swift
+//  FileFlow
+//
+//  Created by Ayman Agamy on 2025-01-08.
+//
+
+import SwiftUI
+
+@main
+struct FileFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
